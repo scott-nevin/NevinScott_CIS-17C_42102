e0c7ab89790b76f9cfe79aa4a13c927ab3df8ed3
scott-nevin <scott.nevin@att.net>
2016-02-24 21:44:28 -0800

    Signed-off-by: scott-nevin <scott.nevin@att.net>

---
+++ Week 1/object_array_attempt/build/Debug/Cygwin_4.x-Windows/RowAry.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin_4.x-Windows/RowAry.o: RowAry.cpp RowAry.h
+
+RowAry.h:
---
+++ Week 1/object_array_attempt/build/Debug/Cygwin_4.x-Windows/Table.o.d
@@ -0,0 +1,5 @@
+build/Debug/Cygwin_4.x-Windows/Table.o: Table.cpp Table.h RowAry.h
+
+Table.h:
+
+RowAry.h:
